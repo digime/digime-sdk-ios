db8732b0446bb4875326b3d0544a52228904f03d
brokenzz <gambit33@gmail.com>
2018-11-28 15:25:12 +0000

    Fix for non-main thread usage warning

--- DigiMeSDK/Core/Classes/DMEAppCommunicator.m
+++ DigiMeSDK/Core/Classes/DMEAppCommunicator.m
@@ -62,27 +62,26 @@ static NSTimeInterval const kCATimerInterval = 0.5;
     
     self.sentAction = action;
     self.sentParameters = parameters;
-    
-    if (![self digiMeAppIsInstalled])
-    {
-        [self presentAppstoreView];
-        return; // We have a listener set up to restore this flow after.
-    }
-    
-    NSURLComponents *components = [NSURLComponents componentsWithURL:[self digiMeBaseURL] resolvingAgainstBaseURL:NO];
-    components.host = action;
-    
-    NSMutableArray *newQueryItems = [NSMutableArray arrayWithArray:components.queryItems] ?: [NSMutableArray array];
-    [newQueryItems addObject:[NSURLQueryItem queryItemWithName:kCASdkVersion value:[[NSBundle bundleForClass:[self class]] objectForInfoDictionaryKey:@"CFBundleShortVersionString"]]];
-    
-    [parameters enumerateKeysAndObjectsUsingBlock:^(NSString * _Nonnull key, NSString * _Nonnull obj, BOOL * _Nonnull stop) {
-        [newQueryItems addObject:[NSURLQueryItem queryItemWithName:key value:obj]];
-    }];
-    components.queryItems = newQueryItems;
-    
-    NSURL *openURL = components.URL;
-    
     dispatch_async(dispatch_get_main_queue(), ^{
+        if (![self digiMeAppIsInstalled])
+        {
+            [self presentAppstoreView];
+            return; // We have a listener set up to restore this flow after.
+        }
+        
+        NSURLComponents *components = [NSURLComponents componentsWithURL:[self digiMeBaseURL] resolvingAgainstBaseURL:NO];
+        components.host = action;
+        
+        NSMutableArray *newQueryItems = [NSMutableArray arrayWithArray:components.queryItems] ?: [NSMutableArray array];
+        [newQueryItems addObject:[NSURLQueryItem queryItemWithName:kCASdkVersion value:[[NSBundle bundleForClass:[self class]] objectForInfoDictionaryKey:@"CFBundleShortVersionString"]]];
+        
+        [parameters enumerateKeysAndObjectsUsingBlock:^(NSString * _Nonnull key, NSString * _Nonnull obj, BOOL * _Nonnull stop) {
+            [newQueryItems addObject:[NSURLQueryItem queryItemWithName:key value:obj]];
+        }];
+        components.queryItems = newQueryItems;
+        
+        NSURL *openURL = components.URL;
+        
         [UIApplication.sharedApplication openURL:openURL options:@{} completionHandler:nil];
     });
 }
