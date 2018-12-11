dfc9ff1ddb031ede30c0b05c8a72ff22dd0b3d0b
Alex Hamilton <alex@digi.me>
2018-12-11 17:24:47 +0000

    DIGI-10751 Unit tests for Comment object

--- DigiMeSDK/Repository/Classes/Objects/Social/Comment.swift
+++ DigiMeSDK/Repository/Classes/Objects/Social/Comment.swift
@@ -23,27 +23,24 @@ public class Comment: NSObject, BaseObjectDecodable {
     }
     
     public let personEntityId: String
-    
     public let baseId: String
-//    public let entityId: String
     public let referenceEntityId: String
     public let referenceEntityType: Int
     public let socialNetworkUserEntityId: String?
-    public let updatedDate: Date
-    
-    public let appId: String
-    public let commentCount: Int
+    public let updatedDate: Date?
+    public let appId: String?
+    public let commentCount: Int?
     public let commentId: String
-    public let commentReplyId: String
-    public let likeCount: Int
-    public let link: String
-    public let metaId: String
-    public let personFileRelativePath: String
-    public let personFileUrl: String
-    public let personFullname: String
+    public let commentReplyId: String?
+    public let likeCount: Int?
+    public let link: String?
+    public let metaId: String?
+    public let personFileRelativePath: String?
+    public let personFileUrl: String?
+    public let personFullname: String?
     public let personUsername: String
     public let privacy: Int
-    public let text: String
+    public let text: String?
     public let userReaction: String?
     
     // MARK: - Raw Representations
@@ -53,20 +50,13 @@ public class Comment: NSObject, BaseObjectDecodable {
     enum CodingKeys: String, CodingKey {
         case accountEntityId = "accountentityid"
         case socialNetworkUserEntityId = "socialnetworkuserentityid"
-//        case accountIdentifier = "accountentityid"
         case createdDate = "createddate"
         case identifier = "entityid"
-//        case objectType = "objecttype"
-        
         case personEntityId = "personentityid"
-        
-//        case accountEntityId = "accountentityid"
         case baseId = "baseid"
-//        case entityId = "entityid"
         case referenceEntityId = "referenceentityid"
         case referenceEntityType = "referenceentitytype"
         case updatedDate = "updateddate"
-        
         case appId = "appid"
         case commentCount = "commentcount"
         case commentId = "commentid"
@@ -81,39 +71,5 @@ public class Comment: NSObject, BaseObjectDecodable {
         case privacy = "privacy"
         case text = "text"
         case userReaction = "userreaction"
-    }
-    
-    override private init() {
-        accountEntityId = ""
-        
-        createdDate = Date()
-        
-        identifier = ""
-        
-//        objectType: CAObjectType
-        
-        personEntityId = ""
-        
-        baseId = ""
-//        entityId = ""
-        referenceEntityId = ""
-        referenceEntityType = 0
-        socialNetworkUserEntityId = ""
-        updatedDate = Date()
-        
-        appId = ""
-        commentCount = 0
-        commentId = ""
-        commentReplyId = ""
-        likeCount = 0
-        link = ""
-        metaId = ""
-        personFileRelativePath = ""
-        personFileUrl = ""
-        personFullname = ""
-        personUsername = ""
-        privacy = 0
-        text = ""
-        userReaction = ""
     }
 }
