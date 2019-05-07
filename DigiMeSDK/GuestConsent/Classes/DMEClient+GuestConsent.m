1fd756ed2f587798208dc755c65eb284f28084b3
Alex Hamilton <alex@digi.me>
2019-05-07 12:29:29 +0100

    PR requested changes

--- DigiMeSDK/GuestConsent/Classes/DMEClient+GuestConsent.m
+++ DigiMeSDK/GuestConsent/Classes/DMEClient+GuestConsent.m
@@ -24,34 +24,6 @@
 
 @implementation DMEClient (GuestConsent)
 
-+ (void)load {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        Class class = [self class];
-        
-        SEL originalSelector = @selector(authorize);
-        SEL swizzledSelector = @selector(checkAuthorizationOptions);
-        
-        Method originalMethod = class_getInstanceMethod(class, originalSelector);
-        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
-        
-        BOOL didAddMethod =
-        class_addMethod(class,
-                        originalSelector,
-                        method_getImplementation(swizzledMethod),
-                        method_getTypeEncoding(swizzledMethod));
-        
-        if (didAddMethod) {
-            class_replaceMethod(class,
-                                swizzledSelector,
-                                method_getImplementation(originalMethod),
-                                method_getTypeEncoding(originalMethod));
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod);
-        }
-    });
-}
-
 #pragma mark - class property accessors
 
 DMEGuestConsentManager *_guestConsentManager;
@@ -81,11 +53,6 @@ PreConsentViewController *_preconsentViewController;
 #pragma mark - authorization
 
 - (void)authorizeGuest
-{
-    [self authorizeGuestWithCompletion:nil];
-}
-
-- (void)checkAuthorizationOptions
 {
     if ([self canOpenDigiMeApp])
     {
