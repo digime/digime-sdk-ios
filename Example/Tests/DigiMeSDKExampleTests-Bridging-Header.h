2c167adb614ce6443fbe12f858f607db80ba9859
Alex Hamilton <alex@digi.me>
2019-08-22 16:04:22 +0100

    PR requested changes

--- Example/Tests/DigiMeSDKExampleTests-Bridging-Header.h
+++ Example/Tests/DigiMeSDKExampleTests-Bridging-Header.h
@@ -1,13 +1,13 @@
 //
-//  DigiMeSDKExampleSwiftTests-Bridging-Header.h
+//  DigiMeSDKExampleTests-Bridging-Header.h
 //  DigiMeSDKExampleSwift
 //
 //  Created on 19/08/2019.
 //  Copyright © 2019 digi.me. All rights reserved.
 //
 
-#ifndef DigiMeSDKExampleSwiftTests_Bridging_Header_h
-#define DigiMeSDKExampleSwiftTests_Bridging_Header_h
+#ifndef DigiMeSDKExampleTests_Bridging_Header_h
+#define DigiMeSDKExampleTests_Bridging_Header_h
 
 #import <DigiMeSDK/DigiMeSDK.h>
 #import <DigiMeSDK/DMECrypto.h>
@@ -15,4 +15,4 @@
 #import <DigiMeSDK/NSData+DMECrypto.h>
 #import <DigiMeSDK/NSString+DMECrypto.h>
 
-#endif /* DigiMeSDKExampleSwiftTests_Bridging_Header_h */
+#endif /* DigiMeSDKExampleTests_Bridging_Header_h */
