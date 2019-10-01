c00c407fa71d7cba5463e16d2746338ba53c3c53
Alex Yushchenko <gambit33@gmail.com>
2019-10-01 10:39:32 +0100

    Consolidated error tests. Added 1 more test case and solidified test expectation. (#127)

--- Example/Tests/Error/DMEErrorTests.swift
+++ Example/Tests/Error/DMEErrorTests.swift
@@ -23,4 +23,17 @@ class DMEErrorTests: XCTestCase {
     func testSDKApiError() {
         XCTAssertEqual("me.digi.sdk.api", DME_API_ERROR)
     }
+    
+    func testAuthErrorWithReference() {
+        let errorReference = "abc123"
+        let expectedErrorMessage = "Unknown authorization error has occurred. Reference: \(errorReference)"
+        let error = NSError.authError(AuthError.general, reference: errorReference)
+        XCTAssert(expectedErrorMessage == error.localizedDescription, "Unexpected error escription")
+    }
+    
+    func testAuthErrorWithNilReference() {
+        let expectedErrorMessage = "Unknown authorization error has occurred."
+        let error = NSError.authError(AuthError.general, reference: nil)
+        XCTAssert(expectedErrorMessage == error.localizedDescription, "Unexpected error escription")
+    }
 }
