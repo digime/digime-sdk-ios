1274cec54f13989cebf8eccbb3fca19880e6c13a
brokenzz <gambit33@gmail.com>
2020-04-03 11:16:46 +0100

    Updated with Gen refy contract

--- Examples/Genrefy/Genrefy/DigimeService.swift
+++ Examples/Genrefy/Genrefy/DigimeService.swift
@@ -16,9 +16,9 @@ class DigimeService {
     
     init() {
         let appId = "qgEUV8iJENRiUkuYF5lLpdsOv7Hp0biy"
-        let contractId = "xv1R4Rc63Od956kHtX5H3SSSAFc9WqpT"
-        let p12FileName = "fJI8P5Z4cIhP3HawlXVvxWBrbyj5QkTF"
-        let p12Password = "monkey periscope"
+        let contractId = "yrg1LktWk2gldVk8atD5Pf7Um4c1LnMs"
+        let p12FileName = "yrg1LktWk2gldVk8atD5Pf7Um4c1LnMs"
+        let p12Password = "digime"
         
         let configuration = DMEPullConfiguration(appId: appId, contractId: contractId, p12FileName: p12FileName, p12Password: p12Password)
         configuration?.debugLogEnabled = true
