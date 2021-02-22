0ac05f8a4dcea7121fdef0ebad844b07a1ed4d09
Mike Eustace <mike@digi.me>
2021-02-22 12:57:29 +0000

    Add example contracts for private sharing

--- Examples/SkeletonSwift/DigiMeSDKExampleSwift/PrivateSharingViewController.swift
+++ Examples/SkeletonSwift/DigiMeSDKExampleSwift/PrivateSharingViewController.swift
@@ -20,14 +20,14 @@ class PrivateSharingViewController: UIViewController {
         #warning("REPLACE 'YOUR_APP_ID' with your App ID. Also don't forget to set the app id in CFBundleURLSchemes.")
         static let appId = "YOUR_APP_ID"
         
-        #warning("REPLACE 'YOUR_CONTRACT_ID' with your Private Sharing contract ID.")
-        static let contractId = "YOUR_CONTRACT_ID"
+        #warning("REPLACE example contract ID with your Private Sharing contract ID.")
+        static let contractId = "fJI8P5Z4cIhP3HawlXVvxWBrbyj5QkTF"
         
-        #warning("REPLACE 'YOUR_P12_PASSWORD' with password provided by digi.me Ltd.")
-        static let p12Password = "YOUR_P12_PASSWORD"
+        #warning("REPLACE example .p12 password with password provided by digi.me Ltd.")
+        static let p12Password = "monkey periscope"
         
-        #warning("REPLACE 'YOUR_P12_FILE_NAME' with .p12 file name (without the .p12 extension) provided by digi.me Ltd.")
-        static let p12FileName = "YOUR_P12_FILE_NAME"
+        #warning("REPLACE example .p12 file name with .p12 file name provided by digi.me Ltd.")
+        static let p12FileName = "fJI8P5Z4cIhP3HawlXVvxWBrbyj5QkTF"
     }
     
     override func viewDidLoad() {
