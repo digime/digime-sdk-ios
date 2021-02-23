7b2c0e64be98aa2185fc36d8cb9666ec41bfca3f
Mike Eustace <mike@digi.me>
2021-02-23 13:02:41 +0000

    Add documentation for Swift files

--- Documentation/generate_docs.sh
+++ Documentation/generate_docs.sh
@@ -1,3 +1,8 @@
-# sourcekitten doc --objc SDKHeaders.h -- -x objective-c -isysroot $(xcrun --show-sdk-path) -I $(pwd) > objcDoc.json
-# sourcekitten doc --module-name DigiMeSDK -- -project ../Examples/SkeletonSwift/Pods/DigiMeSDK.xcodeproj > swiftDoc.json
+sourcekitten doc --objc SDKHeaders.h -- -x objective-c -isysroot $(xcrun --show-sdk-path) -I $(pwd) > objcDoc.json
+sourcekitten doc --module-name DigiMeSDK -- -project ../Examples/SkeletonSwift/Pods/DigiMeSDK.xcodeproj > swiftDoc.json
 jazzy
+#remove generated docset as we don't need it
+rm -rf ../docsets
+#remove generated sourcekitten files
+rm -rf swiftDoc.json
+rm -rf objcDoc.json
