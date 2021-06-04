77ea07cec59664f3628cc75b8de3cbd6d7ed1b47
Mike Eustace <mike@digi.me>
2021-06-04 13:20:17 +0100

    Add new Example app

---
+++ Examples/DigiMeSDKExample/DigiMeSDKExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DigiMeSDKExample
+//
+//  Created by Mike Eustace on 04/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
