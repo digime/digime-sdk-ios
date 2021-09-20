471bca0beb41d2f202b01f7fd12989f189f7ad06
Mike Eustace <mike@digi.me>
2021-09-20 14:11:31 +0100

    Remove old example apps

--- Examples/SkeletonSwift/DigiMeSDKExampleSwiftTests/DigiMeSDKExampleSwiftTests-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  DigiMeSDKExampleSwiftTests-Bridging-Header.h
-//  DigiMeSDKExampleSwift
-//
-//  Created on 24/02/2021.
-//  Copyright © 2021 digi.me. All rights reserved.
-//
-
-#pragma once
