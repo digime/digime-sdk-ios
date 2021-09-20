98ad1521c66b6c8887b12694dc7dbb9e37630dd2
Mike Eustace <mike@digi.me>
2021-09-20 14:14:02 +0100

    Remove old SDK

--- DigiMeSDK/Core/Classes/Callbacks/DMEClientCallbacks.m
+++
@@ -1,9 +0,0 @@
-//
-//  DMEClientCallbacks.m
-//  DigiMeSDK
-//
-//  Created on 18/07/2019.
-//  Copyright © 2019 digi.me Limited. All rights reserved.
-//
-
-NSString * const kFileIdKey = @"me.digi.fileid";
--- DigiMeSDK/DigiMeSDK.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-#include "DigiMeSDK-umbrella.h"
