70a2fbbb37d49029228bca5f95a4f931d7d8cc82
Alex Hamilton <alex.hamilton@hotmail.com>
2022-12-01 11:50:57 +0200

    Add Sources to SPM config

--- Package.swift
+++ Package.swift
@@ -24,7 +24,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DigiMeSDK",
-            dependencies: ["Gzip", "ASN1Decoder"]),
+			dependencies: ["Gzip", "ASN1Decoder"],
+			path: "Sources"),
         .testTarget(
             name: "DigiMeSDKTests",
             dependencies: ["DigiMeSDK"],
